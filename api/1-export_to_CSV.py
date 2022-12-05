629a02ec5813f20025cc2776ca7bed3c52b25318
Romain Bayle <5014@holbertonstudents.com>
2022-12-05 16:58:55 +0100

    doc

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-"""Using what you did in the task #0, extend your Python script to export
-data in the CSV format.
+"""export data in a csv file"""
 
-Requirements:
-
-Records all tasks that are owned by this employee
-Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-File name must be: USER_ID.csv"""
 
 import urllib.request
 import json
